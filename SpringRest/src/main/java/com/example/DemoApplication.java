4b24e665d8d3b7a8c162212e6bc6784aacbf5eb6
exilant <exilant@Abduls-MacBook-Pro.local>
2016-08-31 14:29:42 -0700

    initial commit

---
+++ SpringRest/src/main/java/com/example/DemoApplication.java
@@ -0,0 +1,115 @@
+package com.example;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+		public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
+
+@Component
+class ApplicationRunner implements CommandLineRunner{
+	
+	@Override
+	public void run(String... arg0) throws Exception {
+		
+	}
+	
+	
+}
+
+@RestController
+@RequestMapping("/sample")
+class BookingController {
+	
+	@Autowired
+	BookingRepository bookingRepo;
+	
+	@RequestMapping("/bookings")
+	public @ResponseBody List<Booking> getBookings(){
+		return this.bookingRepo.findAll();
+	}
+	
+}
+
+
+
+@Entity
+class Booking{
+	
+	@GeneratedValue @Id
+	private Long id;
+	@Column(name="booking_name")
+	private String bookingName;
+	
+	
+	public Booking(String bookingName) {
+		super();
+		this.bookingName = bookingName;
+	}
+	
+	public Booking() {
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getBookingName() {
+		return bookingName;
+	}
+
+	public void setBookingName(String bookingName) {
+		this.bookingName = bookingName;
+	}
+}
+	
+	interface BookingRepository extends JpaRepository<Booking, Long>{
+		
+		public default List<Booking> getBookingByName(String name){
+			return this.findAll();
+			
+		}
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+            .inMemoryAuthentication().withUser("user").password("password").roles("USER");
+    }
+    
+}
+	}
+
+	
